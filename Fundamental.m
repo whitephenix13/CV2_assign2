41e7b24f92e6cc0b6ff57e4f19a5306f86896e5b
whitephenix13 <alexandrelasbleis@yahoo.fr>
2017-04-25 16:18:23 +0200

    method 1.3 implemented but not tested

--- Fundamental.m
+++ Fundamental.m
@@ -1,5 +1,6 @@
-function [ F ] = Fundamental( xa, xb, ya, yb )
+function [ F ] = Fundamental( xa, xb, ya, yb,method )
 
+if(strcmp(method,'original'))
 f = ones(length(xa),1);
 A = [(xa.*xb).', (xa.*yb).', xa.', (ya.*xb).', (ya.*yb).', ya.', xb.', yb.', f];
 [U,S,V] = svd(A);
@@ -10,6 +11,35 @@ Fundamental = reshape(V(:,c),3,3);
 [ r, c ] = find(S_i==min(S_i(S_i>0)));
 S_i(r,c)=0;
 F = U_i*S_i*V_i;
-
+elseif(strcmp(method,'normalized'))
+    
+elseif(strcmp(method,'RANSAC'))
+    threshold = 0.1;%TODO: tune it 
+    max_nb_inlier = -1;
+    max_num_iter = 10;
+    %from xa,ya,xb,yb compute the normalized pairs p <-> q
+    %p,q are of size 3,n
+    p=NormalizedFundamental(xa,ya);
+    q=NormalizedFundamental(xb,yb);
+    for i=1:max_num_iter
+        %First pick 8 point correspondences randomly from ^pi(p1) and
+        %^pi'(p2)
+        random_indexes= randperm(size(p,2),8);
+        %p1 and p2 are 3xn n=8
+        rand_p=p(:,random_indexes);
+        rand_q=q(:,random_indexes);
+        %compute normalized fundamental F2 for those 8 matches
+        F2 = Fundamental( rand_p(1,:), rand_q(1,:), rand_p(2,:), rand_q(2,:),'normalized' );
+        %count number of inliers among all points for this fundamental F2
+        d=sampson_distance(p,q,F2);%1xn vector
+        nb_inliers = sum(d<=threshold);
+        %keep in memory F for which the number of inliers is the biggest 
+        if(nb_inliers>max_nb_inlier)
+            max_nb_inlier=nb_inliers;
+            F=F2;
+        end
+    end  
 end
+
+
 
--- main.m
+++ main.m
@@ -4,6 +4,13 @@ function [ F ] = main( img1, img2, points )
 %img1 - image 2
 %points - number of matching pairs to detect
 
+if(nargin==0)
+    im1_nb = '01';
+    im2_nb = '02';
+    points = 10;
+    img1 = strcat('House/frame000000',im1_nb,'.png');
+    img2 = strcat('House/frame000000',im2_nb,'.png');
+end
 %Read input
 img1 = imread(img1);
 img2 = imread(img2);
@@ -13,9 +20,9 @@ img2 = imread(img2);
 
 %Run Ransac, output matching pair coordinates
 [ xa, xb, ya, yb ] = RANSAC2( image1, image2, points );
-
+ 
 %Calculate Fundamental matrix
-[ F ] = Fundamental( xa, xb, ya, yb );
+[ F ] = Fundamental( xa, xb, ya, yb,'original');
 
 end
 

d9047f396e0f0c7650689c5d2c3a3ba4e3400ec4
Vladimir <vladimir.isakov91@yandex.com>
2017-05-11 22:16:37 +0200

    Update SfM.m

--- SfM.m
+++ SfM.m
@@ -1,5 +1,5 @@
 function [ structure, motion, s ] = SfM( measurement_matrix, scale_z, threshold )
-
+%All in lecture 2
 [U,S,V] = svd(measurement_matrix);
 
 U3 = U(:,1:3);
@@ -9,17 +9,18 @@ V3 = Vt(1:3,:);
 
 motion = U3*sqrtm(S3);
 structure = sqrtm(S3)*V3;
+
+%Filter points with big z values
 s = [];
 for i=1:length(structure(1,:))
-    if sum(abs(structure(:,i))>threshold)==0
+    if sum(abs(structure(3,i))>threshold)==0
         s = [s, structure(:, i)];
     end  
 end
+%Scale up z values
 s(3,:) = s(3,:)*scale_z;
 %Plots
-figure(1);
-scatter3(structure(1,:),structure(2,:),structure(3,:));
-figure(2);
+figure(3);
 scatter3(s(1,:),s(2,:),s(3,:));
 end
 

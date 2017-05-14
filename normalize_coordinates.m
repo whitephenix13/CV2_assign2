856a93128ddcb4abd303a735bddd586112df7afa
Vladimir <vladimir.isakov91@yandex.com>
2017-05-14 13:47:25 +0200

    part 3

--- normalize_coordinates.m
+++ normalize_coordinates.m
@@ -1,38 +1,24 @@
-function [ new_point_correspondance ] = normalize_coordinates( point_correspondance, point_view_matrix )
+function [ new_point_correspondance ] = normalize_coordinates( point_correspondance, x1,x2,y1,y2 )
 
 new_point_correspondance = point_correspondance;
-%Number of images
-images=49;
-%Number of points
-points1 = length(point_view_matrix(1,:));
-%For each image
-for i=1:images
+for i=x1:x2
     X = [];
     Y = [];
-    %For each point
-    for k=1:points1
-        %if point is in frame
-        if point_view_matrix(i,k) == 1
-            %Find where indices of columns are equal to point_correspondance(i,:,3), to get points data  
+    for k=y1:y2
+        %if point_view_matrix(i,k) == 1
             index = find(point_correspondance(i,:,3)==k);
-            %Get X and Y coordinates, take only once for each match
             X = [X, point_correspondance(i,index(1),1)];
             Y = [Y, point_correspondance(i,index(1),2)];
-         end
-    end 
-    %Get mean values
+         %end
+    end   
     Xmean = mean(X);
     Ymean = mean(Y);
-    %For each points
-    for k=1:points1
-        %if point is in frame
-        if point_view_matrix(i,k) == 1
-            %Find where indices of columns are equal to point_correspondance(i,:,3), to get points data
+    for k=y1:y2
+        %if point_view_matrix(i,k) == 1
             index = find(point_correspondance(i,:,3)==k);
-            %Normalize by subtracting means from each point in frame
             new_point_correspondance(i,index(1),1) = new_point_correspondance(i,index(1),1) - Xmean;
             new_point_correspondance(i,index(1),2) = new_point_correspondance(i,index(1),2) - Ymean;                     
-         end
+         %end
     end
                  
 end
